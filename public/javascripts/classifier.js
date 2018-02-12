03bbd9fe05bcaa99bafd0f47213ea5429b0aa8e7
Lucas Martin Calderon <lucasmartincalderon1@gmail.com>
2018-02-12 18:38:03 +0100

    Done

--- public/javascripts/classifier.js
+++ public/javascripts/classifier.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
-require('./twitter');
+const Twit = require('twit');
+const config = require('../../config');
+let T = new Twit(config);
+var tweeter = require('./twitter');
 var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
 var natural_language_understanding = new NaturalLanguageUnderstandingV1({
   'username': process.env.USERNAME,
@@ -7,38 +10,47 @@ var natural_language_understanding = new NaturalLanguageUnderstandingV1({
   'version_date': process.env.VERSION_DATE
 });
 
-let params = {
-  q: '*',
-  //geocode: "40.416937,-3.704513,5km",
+let params = { 
+  q: '*', 
   geocode: "40.4893538421231,-3.6827461557,10km",
   lang: "en",
-  count: 20
-  //YYYY-MM-DD
-  //until:
- 
- }
+  count: 2
+}
 
- 
 
-var parameters = {
-  'text': twit(),
-  'features': {
-    'semantic_roles': {},
-    'entities': {
-      'emotion': true,
-      'sentiment': true,
-      'limit': 2,
-    },
-    'keywords': {
-      'emotion': true,
-      'sentiment': true,
-      'limit': 2
-    },
-  }
-};
-natural_language_understanding.analyze(parameters, function(err, response) {
-  if (err)
-    console.log('error:', err);
-  else
-    console.log(JSON.stringify(response, null, 2));
-});
+var twit = T.get('search/tweets', params).then( (info) => {
+  var twitString = "";
+   let tweets = info.data.statuses;
+   for(let i=0; i< tweets.length; i++){
+     twitString = twitString + " " + tweets[i].text.replace(/[&\/\\#,;.+()$~%.'":*?<>{}]/g,"");
+    }
+    console.log(typeof(twitString));
+    return twitString;
+  }).then((data) => {
+    var parameters = {
+      'text': data,
+      'features': {
+        'semantic_roles': {},
+        'entities': {
+          'emotion': true,
+          'sentiment': true,
+          'limit': 2,
+        },
+        'keywords': {
+          'emotion': true,
+          'sentiment': true,
+          'limit': 2
+        },
+      }
+    };
+    natural_language_understanding.analyze(parameters, function(err, response) {
+      if (err)
+      console.log('error:', err);
+    else
+      console.log(JSON.stringify(response, null, 2));
+  });
+ });
+
+
+
+
--- public/javascripts/twitter.js
+++ public/javascripts/twitter.js
@@ -3,32 +3,14 @@ const config = require('../../config');
 let T = new Twit(config);
 let twitString = "";
 
-// let params = {
-//  q: '*',
-//  //geocode: "40.416937,-3.704513,5km",
-//  geocode: "40.4893538421231,-3.6827461557,10km",
-//  lang: "en",
-//  count: 20
-//  //YYYY-MM-DD
-//  //until:
 
-// }
-var twits
-function gotData(err, data, response) {
+var twit = (params) => {
+T.get('search/tweets', params).then( (info) => {
+  let tweets = info.data.statuses;
+  for(let i=0; i< tweets.length; i++){
+    twitString = twitString + " " + tweets[i].text.replace(/[&\/\\#,;.+()$~%.'":*?<>{}]/g,"");
+   }
+   return twitString;
+});
+};
 
- //console.log(data);
- 
- let tweets = data.statuses;
- for(let i=0; i< tweets.length; i++){
-   twitString = twitString + " "+ tweets[i].text.replace(/[&\/\\#,;.+()$~%.'":*?<>{}]/g,"");
-   //console.log("Location: " + tweets[i].user.location);
-   //console.log(tweets[i].user);
- }
- console.log("Lo que nos interesa;");
- console.log(twitString);
- 
-}
-
-T.get('search/tweets', params, gotData);
- 
-console.log(twitString)
